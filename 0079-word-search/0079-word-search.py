8bcbe6c4ed55cdd210c6a3a79e53998dc7fa762a
Rishav kumar <101870417+CodeWithRishav@users.noreply.github.com>
2024-04-03 08:52:28 +0530

    Time: 4648 ms (35.76%), Space: 16.5 MB (71.59%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,25 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        def dfs(i, j, cur): # cur: current char count
+            if cur == len(word):
+                return True
+            if (
+                i < 0
+                or i >= m
+                or j < 0
+                or j >= n
+                or board[i][j] == '0'
+                or word[cur] != board[i][j]
+            ):
+                return False
+            t = board[i][j]
+            board[i][j] = '0' # mark as visited
+            for a, b in [[0, 1], [0, -1], [-1, 0], [1, 0]]:
+                x, y = i + a, j + b
+                if dfs(x, y, cur + 1):
+                    return True
+            board[i][j] = t
+            return False
+
+        m, n = len(board), len(board[0])
+        return any(dfs(i, j, 0) for i in range(m) for j in range(n))
